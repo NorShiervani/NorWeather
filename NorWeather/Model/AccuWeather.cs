719103de71012b838308cb889451363c504ab7cf
Nor Shiervani <nor.shier97@gmail.com>
2019-10-22 20:53:23 +0200

    Importing project

---
+++ NorWeather/Model/AccuWeather.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorWeather.Model
+{
+    public class Headline
+    {
+        public DateTime EffectiveDate { get; set; }
+        public int EffectiveEpochDate { get; set; }
+        public int Severity { get; set; }
+        public string Text { get; set; }
+        public string Category { get; set; }
+        public DateTime EndDate { get; set; }
+        public int EndEpochDate { get; set; }
+        public string MobileLink { get; set; }
+        public string Link { get; set; }
+    }
+
+    public class Minimum
+    {
+        public double Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+    public class Maximum
+    {
+        public double Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+    public class Temperature
+    {
+        public Minimum Minimum { get; set; }
+        public Maximum Maximum { get; set; }
+    }
+
+    public class Day
+    {
+        public int Icon { get; set; }
+        public string IconPhrase { get; set; }
+        public bool HasPrecipitation { get; set; }
+        public string PrecipitationType { get; set; }
+        public string PrecipitationIntensity { get; set; }
+    }
+
+    public class Night
+    {
+        public int Icon { get; set; }
+        public string IconPhrase { get; set; }
+        public bool HasPrecipitation { get; set; }
+        public string PrecipitationType { get; set; }
+        public string PrecipitationIntensity { get; set; }
+    }
+
+    public class DailyForecast
+    {
+        public DateTime Date { get; set; }
+        public int EpochDate { get; set; }
+        public Temperature Temperature { get; set; }
+        public Day Day { get; set; }
+        public Night Night { get; set; }
+        public IList<string> Sources { get; set; }
+        public string MobileLink { get; set; }
+        public string Link { get; set; }
+    }
+
+    public class AccuWeather
+    {
+        public Headline Headline { get; set; }
+        public IList<DailyForecast> DailyForecasts { get; set; }
+    }
+
+}
---
+++ NorWeather/ViewModel/API.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using NorWeather.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorWeather.ViewModel
+{
+    class API
+    {
+        public const string WEATHER_API_KEY = "";
+        public const string RESOURCE_URL = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/{0}?apikey={1}";
+
+        public async Task<AccuWeather> GetWeatherDataAsync(string locationkey)
+        {
+            AccuWeather weatherData = new AccuWeather();
+            string url = string.Format(RESOURCE_URL, locationkey, WEATHER_API_KEY);
+
+            using(HttpClient client = new HttpClient())
+            {
+                var apiresponse = await client.GetAsync(url);
+                string json = await apiresponse.Content.ReadAsStringAsync();
+
+                weatherData = JsonConvert.DeserializeObject<AccuWeather>(json);
+            }
+
+            return weatherData;
+        }
+    }
+}
